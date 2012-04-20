285694ab775e02db050a3f0f61c23331f8c713ae
Vincent Povirk <madewokherd@gmail.com>
2012-04-20 09:10:57 -0500

    Explicitly disable the csharp bindings.
    This causes problems in the mingw build, if we end up in a combined
    mingw/msvc environment somehow (which we shouldn't, but the package without
    --built=true creates this). Anyway, there's no reason to build these in a
    mingw environment instead of msvc.

--- coapp-build.sh
+++ coapp-build.sh
@@ -3,11 +3,11 @@ if (echo "$PWD"|grep -q ' '); then
 	exit 1
 fi
 
-export C_INCLUDE_PATH=$PWD/../libiconv/include
-export CPLUS_INCLUDE_PATH=$PWD/../libiconv/include
-export LIBRARY_PATH=$PWD/../libiconv/lib/.libs
+export C_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
+export CPLUS_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
+export LIBRARY_PATH=$PWD/../libiconv/mingw-build/lib/.libs
 cd gettext-runtime
 mkdir mingw-build
 cd mingw-build
-../configure --enable-threads=win32
+../configure --enable-threads=win32 --disable-csharp
 make all
