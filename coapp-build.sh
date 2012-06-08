8ddf6f87e3884927c4755a817a5d80149b102608
Vincent Povirk <madewokherd@gmail.com>
2012-06-08 14:39:17 -0500

    Temporarily remove dependency on libiconv mingw build.
    To do this properly, we need the ability to find an installed libiconv dll
    in sxs, and a libiconv-dev[vc6] package.

--- coapp-build.sh
+++ coapp-build.sh
@@ -3,11 +3,15 @@ if (echo "$PWD"|grep -q ' '); then
 	exit 1
 fi
 
-export C_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
-export CPLUS_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
-export LIBRARY_PATH=$PWD/../libiconv/mingw-build/lib/.libs
+# The build automation won't let us have dependencies on other source trees.
+# For now, just skip the libiconv dep and build the header, as it's all we ship.
+#export C_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
+#export CPLUS_INCLUDE_PATH=$PWD/../libiconv/mingw-build/include
+#export LIBRARY_PATH=$PWD/../libiconv/mingw-build/lib/.libs
 cd gettext-runtime
 mkdir mingw-build
 cd mingw-build
 ../configure --enable-threads=win32 --disable-csharp
-make all
+#make all
+cd intl
+make libintl.h
