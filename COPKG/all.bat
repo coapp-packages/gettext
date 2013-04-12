2ab2ba21de30ac3e33dd5466d93dce4bd17de275
Vincent Povirk <madewokherd@gmail.com>
2013-04-12 16:43:46 -0500

    Add autopkg file for nuget package.

--- COPKG/all.bat
+++ COPKG/all.bat
@@ -6,6 +6,8 @@ if "%1"=="noclean" (
 	set __NOCLEAN__=true
 	shift)
 
+copy ..\gettext-runtime\intl\libgnuintl.h.in ..\gettext-runtime\intl\libintl.h
+
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 call :build Win32 Release v110
